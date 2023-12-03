ec8f9c39bc2a00f5a4e277010ade4524739dec55
bobyres <124585282+bobyres@users.noreply.github.com>
2023-12-03 19:20:44 +0100

    Add files via upload

---
+++ axolotl-main/compte_rendus/app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, request, jsonify
+from infer import chat  # Importez votre script d'inférence
+
+app = Flask(__name__)
+
+@app.route('/inference', methods=['POST'])
+def inference():
+    data = request.get_json()
+    query = data.get('query', '')
+    history = data.get('history', [])
+    temperature = data.get('temperature', 0.7)  # Exemple de paramètre, ajustez selon vos besoins
+
+    response, updated_history = chat(query, history=history, temperature=temperature)
+
+    return jsonify({'response': response, 'updated_history': updated_history})
+
+if __name__ == '__main__':
+    app.run(debug=True)
